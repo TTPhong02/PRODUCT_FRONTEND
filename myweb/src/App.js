a7a306b29cac6ef4080358a5efcfcef6790968b9
TTPhong02 <Phong762002@gmail.com>
2022-05-28 14:47:37 +0700

    Header-Footer

---
+++ myweb/src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './component/Header/Header';
+import Footer from './component/Footer/Footer';
+
+function App() {
+  return (
+    <div className="App ">
+      <Header/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
