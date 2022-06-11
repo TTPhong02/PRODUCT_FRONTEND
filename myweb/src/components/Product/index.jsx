91cc7f4cdf44e0e82724f714e8a815773917afef
ThoNguyen <hatigr.th@gmail.com>
2022-06-11 15:39:28 +0700

    update home

--- myweb/src/components/Product/index.jsx
+++ myweb/src/components/Product/index.jsx
@@ -4,9 +4,13 @@ import "./product.scss";
 const Product = (props) => {
   const { src, name, price, sale } = props;
   return (
-    <div className="item-product col-span-1">
+    <div className="item-product col-span-1 md:col-span-1 sm:col-span-1">
       <img className="img-product" src={src} alt="" />
-      {sale && <span className="discount-product">{sale}</span>}
+      {sale && (
+        <div className="discount-product">
+          <div className="sale">Giảm {sale}%</div>
+        </div>
+      )}
       <p className="name-product">{name}</p>
       <p className="price-product">{price} vnđ</p>
     </div>
